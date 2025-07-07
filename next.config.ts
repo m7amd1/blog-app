899455fa9ecad5cc2a6a5c1ff9431aafe9575634
Mohammad Khaseeb <mohammadkhaseeb2@outlook.com>
2025-07-07 04:46:01 +0300

    bug fixes: images hostname

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "www.salesforce.com",
         protocol: "https",
-        port: "",
-      },
-      {
-        hostname: "lh3.googleusercontent.com",
-        protocol: "https",
+        hostname: "**",
         port: "",
       },
     ],
